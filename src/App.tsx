3e98d3da2534d26aec70dd96b46fbee009f8d148
JuneOrg2020 <jh.dev.acc.1026.i2k9@gmail.com>
2021-05-02 22:45:19 +0900

    change homepage path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PersistentDrawerLeft from './templates/drawer';
 import GroupPage from './pages/groupPage';
 import StudySettingPage from './pages/studySettingPage';
@@ -23,7 +23,7 @@ class App extends React.PureComponent<PageProps, PageState> {
     checkInit();
 
     return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <PersistentDrawerLeft />
         <div className="content-area">
           <Switch>
@@ -40,7 +40,7 @@ class App extends React.PureComponent<PageProps, PageState> {
           </Switch>
           <div className="footer-area" />
         </div>
-      </Router>
+      </BrowserRouter>
     );
   }
 }
