17478c60eba938dbe4ee04496e49834e44da81ce
JuneOrg2020 <jh.dev.acc.1026.i2k9@gmail.com>
2021-05-02 23:03:58 +0900

    change Router to Hash

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import PersistentDrawerLeft from './templates/drawer';
 import GroupPage from './pages/groupPage';
 import StudySettingPage from './pages/studySettingPage';
@@ -23,7 +23,7 @@ class App extends React.PureComponent<PageProps, PageState> {
     checkInit();
 
     return (
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <PersistentDrawerLeft />
         <div className="content-area">
           <Switch>
@@ -36,11 +36,11 @@ class App extends React.PureComponent<PageProps, PageState> {
             <Route path="/edit/:groupId" component={EditPage} />
             <Route path="/save/" component={SavePage} />
             <Route path="/load/" component={LoadPage} />
-            <Route path="*" component={GroupPage} />
+            <Route component={GroupPage} />
           </Switch>
-          <div className="footer-area" />
+          <div className="footer-area"/>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
